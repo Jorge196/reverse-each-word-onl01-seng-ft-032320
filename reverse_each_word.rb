8cb0b80cb139721b2cd212f63ac4b8be427431d3
Jorge Ramirez <joramirez96@gmail.com>
2020-03-29 19:57:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
-def reverse_each_word("Hello there, and how are you?")
-  words.reverse_each("Hello there, and how are you?")
+def reverse_each_word(string)
+  word_list = string.split 
+  word_list.each do |w|
+    w.length
+  end 
 end
