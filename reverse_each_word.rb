b559a8da165f6073bf8986d0716a5a1c30d8e8b3
Jorge Ramirez <joramirez96@gmail.com>
2020-03-29 20:35:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,19 +2,18 @@ def reverse_each_word(string)
   original_array = string.split (" ")
   return_array = []
   original_array.each do |string|
-    return_array << string.reverse 
+    return_array << string.reverse
   end
   return_array.join(" ")
-end 
+end
 
 def reverse_each_word(string)
   array = string.split (" ")
   test_array = []
-  array.collect do |string| 
-    test_array << string.reverse 
-  end 
+  array.collect do |string|
+    test_array << string.reverse
+  end
   test_array.join(" ")
-end 
+end
 
 reverse_each_word("Hello ther, and how are you?")
-
